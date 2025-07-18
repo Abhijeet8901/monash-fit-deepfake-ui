57e3bf24e797ba099ed6eecf3609c83bf09354c4
Abhijeet Narang <abhijeetnarang.nr@gmail.com>
2025-07-18 13:04:26 +1000

    Updated gradio links

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -1,5 +1,5 @@
-export const FAKESHIELD_DTE_BASE_URL = 'https://bee929a81dfc108acc.gradio.live/'
+export const FAKESHIELD_DTE_BASE_URL = 'https://fbb3048cb6d2249a49.gradio.live'
 
 export const FAKESHIELD_MFLM_BASE_URL = 'https://08db946354ed329756.gradio.live'
 
-export const STEP1X_BASE_URL = 'https://6d1b33a64ec300097d.gradio.live'
+export const STEP1X_BASE_URL = 'https://2f97336d81820e4436.gradio.live'
